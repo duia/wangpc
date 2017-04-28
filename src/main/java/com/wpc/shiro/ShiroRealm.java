470323e3db906bfe9a5fd779a3168c105a0f11c0
王鹏程 <wpcfree@qq.com>
2017-04-28 18:19:55 +0800

    shiro添加 and or not扩展

--- src/main/java/com/wpc/shiro/ShiroRealm.java
+++ src/main/java/com/wpc/shiro/ShiroRealm.java
@@ -25,6 +25,10 @@ import com.wpc.admin.entity.User;
 
 public class ShiroRealm extends AuthorizingRealm {
 
+    private static final String OR_OPERATOR = " or ";
+    private static final String AND_OPERATOR = " and ";
+    private static final String NOT_OPERATOR = "not ";
+
     @Resource(name = UserDao.BEAN_ID)
     private UserDao userDao;
     @Resource(name = AuthRoleDao.BEAN_ID)
@@ -109,6 +113,42 @@ public class ShiroRealm extends AuthorizingRealm {
             if (null != session) {
                 session.setAttribute(key, value);
             }
+        }
+    }
+
+    /**
+     * 支持or and not 关键词  不支持and or混用
+     * @param principals
+     * @param permission
+     * @return
+     */
+    public boolean isPermitted(PrincipalCollection principals, String permission) {
+        if(permission.contains(OR_OPERATOR)) {
+            String[] permissions = permission.split(OR_OPERATOR);
+            for(String orPermission : permissions) {
+                if(isPermittedWithNotOperator(principals, orPermission)) {
+                    return true;
+                }
+            }
+            return false;
+        } else if(permission.contains(AND_OPERATOR)) {
+            String[] permissions = permission.split(AND_OPERATOR);
+            for(String orPermission : permissions) {
+                if(!isPermittedWithNotOperator(principals, orPermission)) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return isPermittedWithNotOperator(principals, permission);
+        }
+    }
+
+    private boolean isPermittedWithNotOperator(PrincipalCollection principals, String permission) {
+        if(permission.startsWith(NOT_OPERATOR)) {
+            return !super.isPermitted(principals, permission.substring(NOT_OPERATOR.length()));
+        } else {
+            return super.isPermitted(principals, permission);
         }
     }
 
