1897856c3ad6989cac2e89e5aa7097ed42b4a805
王鹏程 <wpcfree@qq.com>
2017-07-08 23:39:58 +0800

    动态 注解

--- src/main/java/com/wpc/design_patterns/proxy/DynamicProxyTest.java
+++ src/main/java/com/wpc/design_patterns/proxy/DynamicProxyTest.java
@@ -17,15 +17,17 @@ public class DynamicProxyTest {
          * interfaces:　　一个Interface对象的数组，表示的是我将要给我需要代理的对象提供一组什么接口，如果我提供了一组接口给它，那么这个代理对象就宣称实现了该接口(多态)，这样我就能调用这组接口中的方法了
          * handler:　　一个InvocationHandler对象，表示的是当我这个动态代理对象在调用方法的时候，会关联到哪一个InvocationHandler对象上
          */
+        Subject subject = (Subject) Proxy.newProxyInstance(handler.getClass().getClassLoader(), realSubject
+                .getClass().getInterfaces(), handler);
         /*
          * 通过Proxy的newProxyInstance方法来创建我们的代理对象，我们来看看其三个参数
          * 第一个参数 handler.getClass().getClassLoader() ，我们这里使用handler这个类的ClassLoader对象来加载我们的代理对象
          * 第二个参数realSubject.getClass().getInterfaces()，我们这里为代理对象提供的接口是真实对象所实行的接口，表示我要代理的是该真实对象，这样我就能调用这组接口中的方法了
          * 第三个参数handler， 我们这里将这个代理对象关联到了上方的 InvocationHandler 这个对象上
          */
-        Subject subject = (Subject) Proxy.newProxyInstance(handler.getClass().getClassLoader(), realSubject
-                .getClass().getInterfaces(), handler);
 
+        //返回的对象既不是Subject类型的对象，也不是InvocationHandler的对象
+        //而是$Proxy0类似的代理对象
         System.out.println(subject.getClass().getName());
         subject.rent();
         subject.hello("world");
